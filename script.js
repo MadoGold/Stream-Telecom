22571be21519568f223991698f1b71698da50b44
MadoGold <madogold2002@gmail.com>
2023-06-02 19:04:18 +0300

    Fix close popup, fix underline

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const openPopup = document.getElementById('TwoFactorAuth');
 const closePopup = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
+const popupBody = document.querySelector('.popup__body');
 
 openPopup.addEventListener('click', (e) => {
   e.preventDefault();
@@ -10,5 +11,14 @@ openPopup.addEventListener('click', (e) => {
 closePopup.addEventListener('click', (e) => {
   e.preventDefault();
   popup.classList.remove('open');
+});
+
+popup.addEventListener('click', (e) => {
+  e.preventDefault();
+  popup.classList.remove('open');
+});
+
+popupBody.addEventListener('click', (e) => {
+  e.stopPropagation();
 });
 
